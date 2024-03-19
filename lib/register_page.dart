82e86931ab66fe050247199fb82bbb3b679e12a0
dev-vickie <vmutethia84@gmail.com>
2024-03-19 13:29:22 +0300

    Firebase Integration Demo

---
+++ lib/register_page.dart
@@ -0,0 +1,131 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_build_context_synchronously
+
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:firebase_auth/firebase_auth.dart";
+import "package:firebase_test/home.dart";
+import "package:flutter/material.dart";
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  //create controllers for the text fields
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final phoneController = TextEditingController();
+  final userController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    phoneController.dispose();
+    userController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register Page"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: userController,
+              decoration: InputDecoration(
+                labelText: "Username",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: phoneController,
+              decoration: InputDecoration(
+                labelText: "Phone Number",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            OutlinedButton(
+              onPressed: () async {
+                try {
+                  await FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                    email: emailController.text,
+                    password: passwordController.text,
+                  )
+                      .then((credentials)async {
+                   await  FirebaseFirestore.instance
+                        .collection("users")
+                        .doc(credentials.user!.uid)
+                        .set({
+                      "uid": credentials.user!.uid,
+                      "username": userController.text,
+                      "email": emailController.text,
+                      "phone": phoneController.text,
+                      
+
+                    }).then(
+                      (value) => Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => Homepage(),
+                        ),
+                      ),
+                    );
+                  });
+                } catch (e) {
+                  debugPrint(e.toString());
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        e.toString(),
+                      ),
+                    ),
+                  );
+                }
+              },
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.green,
+                minimumSize: Size(300, 50),
+              ),
+              child: Text("Register"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
